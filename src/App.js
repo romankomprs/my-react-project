01be0d9e3375ee5f205e291dd20308322d96d46a
romankomprs <rkomprs@gmail.com>
2021-10-12 12:22:58 +0200

    added eslint and prettier to my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <p>
+        In order to run eslint and prettier together I need to install
+        compatible version. In 2021 in summer these two versions work without
+        any problems as far as I know.
+      </p>
+      <p>npm install -D eslint@7.18.0 eslint-config-prettier@8.1.0 </p>
     </div>
   );
 }
